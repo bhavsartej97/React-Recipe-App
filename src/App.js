57a45f1c30f79bd86e442cd956e92bf150c5e5e0
Tej Bhavsar <bhavsartej97@gmail.com>
2019-08-02 11:35:42 -0400

    setup complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div>
+      Hello World !!!
+    </div>
+  );
+}
+
+export default App;
